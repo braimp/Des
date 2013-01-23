9baf57d27d2b9492c8fe9cbe49d82b039e0dc729
lujiefeng <lujfsd@163.com>
2013-01-23 21:09:24 +0800

    DES
    DES加密算法及其原理

---
+++ des.h
@@ -0,0 +1,7 @@
+#ifndef __DES_H
+#define __DES_H
+
+void des(unsigned char *Key,unsigned char *datain,unsigned char *dataout);
+void undes(unsigned char *Key,unsigned char *datain,unsigned char *dataout);
+
+#endif
